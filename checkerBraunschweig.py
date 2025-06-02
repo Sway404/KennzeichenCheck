09a45d9d175166230e8b3ae9468a9558a91e6586
Sway404 <78591247+Sway404@users.noreply.github.com>
2025-06-02 15:46:20 +0200

    Update checkerBraunschweig.py

--- checkerBraunschweig.py
+++ checkerBraunschweig.py
@@ -24,7 +24,7 @@ def check_plates():
     bot_token = os.getenv("TELEGRAM_BOT_TOKEN")
     chat_id = os.getenv("TELEGRAM_CHAT_ID")
     chars = "??"
-    nums = "?"
+    nums = "1"
 
     with sync_playwright() as p:
         browser = p.chromium.launch(headless=True)
