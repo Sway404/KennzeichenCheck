af9460abd495cc84802f120201608a8c3ec1ba85
Sway404 <78591247+Sway404@users.noreply.github.com>
2025-06-02 15:18:52 +0200

    Update checkerPeine.py

--- checkerPeine.py
+++ checkerPeine.py
@@ -5,20 +5,28 @@ from pathlib import Path
 import requests
 from playwright.sync_api import sync_playwright
 
-def send_telegram_image(token, chat_id, image_path):
+def send_telegram_photo_with_caption(token, chat_id, image_path, caption):
     url = f"https://api.telegram.org/bot{token}/sendPhoto"
     with open(image_path, "rb") as image:
         files = {"photo": image}
-        data = {"chat_id": chat_id}
+        data = {
+            "chat_id": chat_id,
+            "caption": caption,
+            "parse_mode": "Markdown"
+        }
         response = requests.post(url, files=files, data=data)
     return response.ok
 
 def check_license_plate():
-    bot_token = os.getenv("TELEGRAM_BOT_TOKEN")
-    chat_id = os.getenv("TELEGRAM_CHAT_ID")
     delay = random.randint(1, 5 * 60)
     print(f"We're waiting {delay} seconds")
     time.sleep(delay)
+
+    bot_token = os.getenv("TELEGRAM_BOT_TOKEN")
+    chat_id = os.getenv("TELEGRAM_CHAT_ID")
+    chars = "??"
+    nums = "1"
+    
     with sync_playwright() as p:
         browser = p.chromium.launch(headless=True)
         page = browser.new_page()
@@ -30,8 +38,8 @@ def check_license_plate():
             page.click("button:has-text('Weiter')")
 
         page.wait_for_selector('input[name="WKZSEARCH_CHARS"]')
-        page.fill('input[name="WKZSEARCH_CHARS"]', "??")
-        page.fill('input[name="WKZSEARCH_NUMS"]', "1")
+        page.fill('input[name="WKZSEARCH_CHARS"]', chars)
+        page.fill('input[name="WKZSEARCH_NUMS"]', nums)
 
         page.keyboard.press("Enter")
 
@@ -40,12 +48,12 @@ def check_license_plate():
         if "keine freien" in page.content():
             print("No plates available.")
         else:
+            script_dir = os.path.dirname(os.path.abspath(__file__))
+            screenshot_path = os.path.join(script_dir, "screenshot.png")
             page.click('select[name="WKZRESULTLIST_WKZ"]')
-            desktop = Path.home() / "Desktop"
-            image_path = desktop / "screenshot.png"
-            page.screenshot(path=str(image_path))
-
-        send_telegram_image(bot_token, chat_id, image_path)
+            page.screenshot(path=screenshot_path)
+            caption = "https://wkz.landkreis-peine.de/wkz/?renderer=responsive"
+            send_telegram_photo_with_caption(bot_token, chat_id, screenshot_path, caption)
         browser.close()
 
 check_license_plate()
